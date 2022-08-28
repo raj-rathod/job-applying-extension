da50afd59b8b47397fdb74c691054000c66f0d16
Rajesh Rathore <rajeshrathore05011998@gmail.com>
2022-08-28 23:23:30 +0530

    feat:<chrome storage> chrome storage get and set data

--- src/Shared/constant/constant.ts
+++ src/Shared/constant/constant.ts
@@ -2,3 +2,4 @@ export const defaultIcon = 'assets/default-career.png';
 export const deleteIcon = 'assets/delete.svg';
 export const saveIcon = 'assets/save.svg';
 export const doneIcon = 'assets/done.svg';
+export const All_Saved_Tabs = 'allSavedTabs';
--- src/Utils/tabs.utils.ts
+++ src/Utils/tabs.utils.ts
@@ -1,6 +1,26 @@
-import Tab = chrome.tabs.Tab;
+import { All_Saved_Tabs } from "src/Shared/constant/constant";
+import { Tab } from "src/Shared/interfaces/tab.interface";
+import defaultTab = chrome.tabs.Tab;
 
-export const getCurrentTabs = async(): Promise<Tab[]> => {
+export const getCurrentTabs = async(): Promise<defaultTab[]> => {
     const tabs = await  chrome.tabs.query({},); 
     return tabs;   
 }
+
+export const getAllSavedtabsFromStorage = async (): Promise<
+  Tab[]
+> => {
+    let allSavedTabs: Tab[] = [];
+   await chrome.storage.sync.get(
+    All_Saved_Tabs, (items)=>{
+       allSavedTabs = items[All_Saved_Tabs];
+    }
+  );
+  return allSavedTabs;
+};
+
+export const setAllTabsInStorage = async (
+  tabs: Tab[]
+): Promise<void> => {
+  return chrome.storage.sync.set({ [All_Saved_Tabs]: tabs });
+};
